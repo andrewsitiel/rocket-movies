1d102dd7ce81cd1c0a3d0d0986464dc420a61a66
Andrews Itiel <andrewscarvalho2001@gmail.com>
2022-11-24 21:46:04 -0300

    Make SignUp integration

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,13 +1,47 @@
-import { FiUser, FiMail, FiLock, FiArrowLeft } from "react-icons/fi";
-import background from "../../assets/cinema.png"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { api } from  "../../services/api";
 
+import { FiUser, FiMail, FiLock, FiArrowLeft } from "react-icons/fi";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { Link } from "../../components/Link";
 
+import background from "../../assets/cinema.png";
 import { Container } from "./styles";
 
 export function SignUp () {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit (event) {
+    event.preventDefault();
+
+    if(!name || !email || !password) {
+      return alert("Por favor, preencha todos os campos")
+    }
+    
+    try {
+      
+      const data = {name, email, password}
+
+      await api.post("/users", data);
+      alert("Usuário cadastrado com sucesso");
+      navigate("/");
+
+    } catch (error) {
+
+      if(error){
+        alert(error.message);
+      } else {
+        alert("Não foi possível cadastrar");
+      }
+
+    }
+  }
+
   return (
     <Container>
       <main>
@@ -16,13 +50,13 @@ export function SignUp () {
 
       <form>
         <legend>Crie sua conta</legend>
-        <Input title="Digite seu nome" icon={FiUser} type="text"/>
-        <Input title="exemplo@mail.com" icon={FiMail} type="email"/>
-        <Input title="Digite sua senha" icon={FiLock} type="password"/>
-        <Button title="Cadastrar" type="submit"/>
-      </form>
+        <Input title="Digite seu nome" icon={FiUser} type="text" onChange={(e) => {setName(e.target.value)}} />
+        <Input title="exemplo@mail.com" icon={FiMail} type="email" onChange={(e) => {setEmail(e.target.value)}}/>
+        <Input title="Digite sua senha" icon={FiLock} type="password" onChange={(e) => {setPassword(e.target.value)}}/>
+        <Button title="Cadastrar" type="submit" onClick={(e) => {handleSubmit(e)}}/>
+      </form >
 
-        <Link href="/" title="Voltar para o login" icon={FiArrowLeft}/>
+      <Link href="/" title="Voltar para o login" icon={FiArrowLeft}/>
     </main>
 
     <img src={background} alt="Cinema room" />
